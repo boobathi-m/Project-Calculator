6b25e83a4e791fd1efc5572bfe9e6a3285057bb1
boobathi-m <boobathisundar@gmail.com>
2023-02-05 10:50:15 +0530

    basic calculator codes added

---
+++ src/org/simplecalc/calc.java
@@ -0,0 +1,38 @@
+package org.simplecalc;
+import java.util.Scanner;
+public class calc {
+	static int a, b;
+	public void add() {
+		int c = a + b;
+		System.out.println("Adding both Numbers:"+c);
+	}
+	public void sub() {
+		int c = a - b;
+		System.out.println("Subracting both Numbers:"+c);
+	}
+	public void divide() {
+		double d = a;
+		double c = d / b;
+		System.out.println("Dividing both Numbers:"+c);
+	}
+	public void multiply() {
+		int c = a * b;
+		System.out.println("Multiplying both Numbers:"+c);
+	}
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter first number:");
+		a = s.nextInt();
+		System.out.println("Enter first number:");
+		b = s.nextInt();
+		System.out.println("first no:"+a);
+		System.out.println("second  no:"+b);
+		
+		calc c = new calc();
+		c.add();
+		c.sub();
+		c.divide();
+		c.multiply();
+	}
+}
