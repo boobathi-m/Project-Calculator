198c83ded7294c0df68b1f6a3920e9e848a8cb35
boobathi-m <boobathisundar@gmail.com>
2023-03-02 15:39:43 +0530

    operation titles added

--- src/org/simplecalc/calc.java
+++ src/org/simplecalc/calc.java
@@ -1,34 +1,41 @@
 package org.simplecalc;
+
 import java.util.Scanner;
+
 public class calc {
 	static int a, b;
+
 	public void add() {
 		int c = a + b;
-		System.out.println("Adding both Numbers:"+c);
+		System.out.println("Adding both Numbers:" + c);
 	}
+
 	public void sub() {
 		int c = a - b;
-		System.out.println("Subracting both Numbers:"+c);
+		System.out.println("Subracting both Numbers:" + c);
 	}
+
 	public void divide() {
 		double d = a;
 		double c = d / b;
-		System.out.println("Dividing both Numbers:"+c);
+		System.out.println("Dividing both Numbers:" + c);
 	}
+
 	public void multiply() {
 		int c = a * b;
-		System.out.println("Multiplying both Numbers:"+c);
+		System.out.println("Multiplying both Numbers:" + c);
 	}
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		System.out.println("perfoms  addition ,subraction,division,multiplication");
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter first number:");
 		a = s.nextInt();
 		System.out.println("Enter first number:");
 		b = s.nextInt();
-		System.out.println("first no:"+a);
-		System.out.println("second  no:"+b);
-		
+		System.out.println("first no:" + a);
+		System.out.println("second  no:" + b);
+
 		calc c = new calc();
 		c.add();
 		c.sub();
